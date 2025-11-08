e7d5f198f948183ee1ac2a2a06802ed10654051d
sriharieetyala <sriharieetyala143@gmail.com>
2025-11-08 10:20:07 +0530

    Initial commit - Maven project

---
+++ src/test/java/MathCalvulationTest.java
@@ -0,0 +1,19 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+public class MathCalvulationTest {
+	@Test
+		public void testAd() {
+			int i=10;
+			int j=20;
+			assertEquals(40,40);
+			assertNotNull("hello");
+			System.out.println("Test is sussessful");
+		}
+	@Test
+	public void testMulti() {
+		System.out.println("Test is sussessful with multipilucation");
+	}
+}
